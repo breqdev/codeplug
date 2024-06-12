b8758386f78764c8444c70b7fa2683a94eade550
Brooke Chalmers <breq@breq.dev>
2024-06-12 11:00:10 -0400

    add CSV format for frs, gmrs, marine, weather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import yaml
+import csv
 import inquirer
 
 regions = os.listdir("repeaters")
@@ -7,11 +8,10 @@ regions = os.listdir("repeaters")
 questions = [
     inquirer.Checkbox("regions", "Which regions would you like to include?", regions),
     inquirer.List("weather", "Would you like to include weather radio? (NOAA Weather Alert and Weatheadio)", ["Yes", "No"]),
-    inquirer.List("gmrs", "Would you like to include GMRS channels?", ["Yes", "No"]),
+    inquirer.List("gmrs", "Would you like to include GMRS channels?", ["All", "Simplex", "Repeaters", "None"]),
     inquirer.List("frs", "Would you like to include FRS channels?", ["Yes", "No"]),
     inquirer.List("murs", "Would you like to include MURS channels?", ["Yes", "No"]),
-    inquirer.List("aar", "Would you like to include AAR (railroad) channels?", ["Yes", "No"]),
-    inquirer.List("marine", "Would you like to include Marine VHF channels?", ["Yes", "No"]),
+    inquirer.List("marine", "Would you like to include Marine VHF channels?", ["Yes", "Most Common", "No"]),
 ]
 
 answers = inquirer.prompt(questions)
